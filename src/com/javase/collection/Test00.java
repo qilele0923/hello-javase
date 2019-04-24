3d1e48925f38ce61cd7f82f95c075ea317f46688
qilele0923 <691417830@qq.com>
2019-04-24 10:57:26 +0800

    多添加了一行

--- src/com/javase/collection/Test00.java
+++ src/com/javase/collection/Test00.java
@@ -6,5 +6,7 @@ package com.javase.collection;
 public class Test00 {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("gggg");
     }
 }
