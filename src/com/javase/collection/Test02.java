0a8848dd57d7b69a5572d2d3694af989b160679f
qilele0923 <691417830@qq.com>
2019-04-24 11:29:46 +0800

    初始化操作

--- src/com/javase/collection/Test02.java
+++ src/com/javase/collection/Test02.java
@@ -3,9 +3,8 @@ package com.javase.collection;
 /**
  * Created by qi on 2019/4/24 0024.
  **/
-public class Test00 {
+public class Test02 {
     public static void main(String[] args) {
-        System.out.println("hello world");
-
+        System.out.println("java");
     }
 }
