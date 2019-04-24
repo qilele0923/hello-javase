176e9fee6746327673b2302b086e5cdd1cce5ce1
qilele0923 <691417830@qq.com>
2019-04-24 10:53:26 +0800

    第一个测试类

---
+++ src/com/javase/collection/Test00.java
@@ -0,0 +1,10 @@
+package com.javase.collection;
+
+/**
+ * Created by qi on 2019/4/24 0024.
+ **/
+public class Test00 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
